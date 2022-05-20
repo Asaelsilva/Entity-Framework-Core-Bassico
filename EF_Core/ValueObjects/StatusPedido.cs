b2bb38069c61d66812f4de1ca3ed4314d83379d0
Asael Silva <asael.guitar_g3@hotmail.com>
2022-05-20 14:23:30 -0300

    Criando Enums

---
+++ EF_Core/ValueObjects/StatusPedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core.ValueObjects
+{
+    public enum StatusPedido
+    {
+        Analise, 
+        Finalizado,
+        Entregue,
+    }
+}
---
+++ EF_Core/ValueObjects/TipoFrete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core.ValueObjects
+{
+    public enum TipoFrete
+    {
+        CIF,
+        FOB,
+        SemFrete,
+    }
+}
