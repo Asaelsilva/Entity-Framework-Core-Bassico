f84e7f3c704d6a31a25a16d7ac14bdd188847615
Asael Silva <asael.guitar_g3@hotmail.com>
2022-05-23 12:00:53 -0300

    Criando classe de contexto e realizando mapeamento através do Fluent API

---
+++ EF_Core/Data/ApplicationContext.cs
@@ -0,0 +1,70 @@
+﻿using EF_Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core.Data
+{
+    public class ApplicationContext : DbContext
+    {       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=DESKTOP-E79S2CD\\SQL_ASAEL; Initial Catalog= CursoEFCoreBasico_DIO; Integrated Security= True");
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(p =>
+            {
+                p.ToTable("Cliente");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
+                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+
+                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+
+            });
+
+            modelBuilder.Entity<Produto>(p =>
+            {
+                p.ToTable("Produto");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(100)").IsRequired();
+                p.Property(p => p.Descricao).HasColumnType("VARCHAR(150)").IsRequired();
+                p.Property(p => p.Valor).IsRequired();
+                p.Property(p => p.TipoProduto).HasConversion<string>();
+                p.Property(p => p.Ativo);
+            });
+
+            modelBuilder.Entity<Pedido>(p =>
+            {
+                p.ToTable("Pedido");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE(100)").ValueGeneratedOnAdd();
+                p.Property(p => p.StatusPedido).HasConversion<string>();
+                p.Property(p => p.TipoFrete).HasConversion<int>();
+                p.Property(p => p.Observacao).HasColumnType("VARCHAR(200)");
+
+                p.HasMany(p => p.Itens)
+                    .WithOne(p => p.Pedido)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<PedidoItem>(p =>
+            {
+                p.ToTable("PedidoItens");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired();
+                p.Property(p => p.Valor).IsRequired();
+                p.Property(p => p.Desconto).IsRequired();
+            });
+        }
+    }
+}
