01e53d130eeee26721849930995cc751b48ba6de
Asael Silva <asael.guitar_g3@hotmail.com>
2022-05-23 14:17:24 -0300

    Separando classes criando arquivos de configuração para o EF Core

--- EF_Core/Data/ApplicationContext.cs
+++ EF_Core/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using EF_Core.Domain;
+﻿using EF_Core.Data.Configurations;
+using EF_Core.Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,53 +19,14 @@ namespace EF_Core.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(p =>
-            {
-                p.ToTable("Cliente");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
-                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
-                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
-                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+            //Instanciando classe por classe dos orquivos de configurtação
+            //modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            //modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            //modelBuilder.ApplyConfiguration(new PedidoItemConfiguration());
+            //modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
 
-                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
-
-            });
-
-            modelBuilder.Entity<Produto>(p =>
-            {
-                p.ToTable("Produto");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(100)").IsRequired();
-                p.Property(p => p.Descricao).HasColumnType("VARCHAR(150)").IsRequired();
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.TipoProduto).HasConversion<string>();
-                p.Property(p => p.Ativo);
-            });
-
-            modelBuilder.Entity<Pedido>(p =>
-            {
-                p.ToTable("Pedido");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE(100)").ValueGeneratedOnAdd();
-                p.Property(p => p.StatusPedido).HasConversion<string>();
-                p.Property(p => p.TipoFrete).HasConversion<int>();
-                p.Property(p => p.Observacao).HasColumnType("VARCHAR(200)");
-
-                p.HasMany(p => p.Itens)
-                    .WithOne(p => p.Pedido)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<PedidoItem>(p =>
-            {
-                p.ToTable("PedidoItens");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired();
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.Desconto).IsRequired();
-            });
+            //Ira procurar todas as classes concretas que estão implementando a Interface IEntityTypeConfiguration neste Assembly.
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
---
+++ EF_Core/Data/Configurations/ProdutoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EF_Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core.Data.Configurations
+{
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produto");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(100)").IsRequired();
+            builder.Property(p => p.Descricao).HasColumnType("VARCHAR(150)").IsRequired();
+            builder.Property(p => p.Valor).IsRequired();
+            builder.Property(p => p.TipoProduto).HasConversion<string>();
+            builder.Property(p => p.Ativo);
+        }
+    }
+}
